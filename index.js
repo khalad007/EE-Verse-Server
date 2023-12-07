1d3fcb9a41e6e34ffc924140884e650f049e90ce
khalad007 <www.arifhossain49919@gmail.com>
2023-12-06 19:54:48 -0800

    Some Modify

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect(); as instructed
 
         const productCollection = client.db('brandDB').collection('products');
         const brandCollection = client.db('brandDB').collection('brand');
@@ -129,8 +129,8 @@ async function run() {
         })
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
